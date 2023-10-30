12f009eaee78316332df979bda4cb6c8e15d2665
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-10-31 01:31:49 +0500

    commits

--- sinup/Windowss/Selling_Forms.xaml.cs
+++ sinup/Windowss/Selling_Forms.xaml.cs
@@ -1,35 +1,23 @@
 ﻿using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using sinup.ServiceLayer.Data;
-using Microsoft.EntityFrameworkCore;
 using ProjectTWO.ServiceLayer.Model;
 using sinup;
+using sinup.ServiceLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ProjectTWO.Windowss
 {
-    /// <summary>
-    /// Interaction logic for Selling_Forms.xaml
-    /// </summary>
+  
     public partial class Selling_Forms : Window
     {
 
         DateTime datetime = DateTime.Now;
-        
-        
+
+
         NpgsqlConnection sqlConnection = new NpgsqlConnection("Server = localhost; Port=5432; User Id = postgres; Password=7788;Database=Project1;");
         public Selling_Forms()
         {
@@ -56,7 +44,7 @@ namespace ProjectTWO.Windowss
                 Price = p.Price,
                 Quantity = p.Quantity,
                 ProdCat = p.ProdCat
-                
+
 
 
             }).ToList();
@@ -79,7 +67,7 @@ namespace ProjectTWO.Windowss
                 ProdName = s.ProdName,
                 ProdPrice = s.ProdPrice,
                 ProdQty = s.ProdQty,
-                Total= s.Total,
+                Total = s.Total,
 
             }).ToList();
             datagrid.ItemsSource = Sellings;
@@ -99,7 +87,7 @@ namespace ProjectTWO.Windowss
                 foreach (var name in names)
                 {
                     category_Combobox.Items.Add(name);
-                    
+
                 }
 
             }
@@ -120,7 +108,7 @@ namespace ProjectTWO.Windowss
         private void category_Combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             GetTable();
-           
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -132,7 +120,7 @@ namespace ProjectTWO.Windowss
 
         private void Add_btn_Click(object sender, RoutedEventArgs e)
         {
-            
+
 
             try
             {
@@ -153,18 +141,11 @@ namespace ProjectTWO.Windowss
                         product.ProdQty = product1.Quantity;
                         product.ProdName = product1.Name;
                         product.Total = (int.Parse(Price_text.Text) * product1.Quantity);
-                        
+
 
                     }
 
-                    //product.ProdName = Name1_textbox.Text;
-                    //product.ProdPrice = int.Parse(Price_text.Text);
-                    //product.ProdQty = int.Parse(Quantity_text.Text);
-                    //int sum;
-                    //int son = int.Parse(Quantity_text.Text);
-                    //int son1 = int.Parse(Price_text.Text);
-                    //sum = son * son1;
-                    //product.Total = sum ;
+
                     db.Add(product);
                     db.SaveChanges();
                     List<Sellings_product> Products = db.selings_products.Select(p => new Sellings_product
@@ -173,11 +154,11 @@ namespace ProjectTWO.Windowss
                         ProdName = p.ProdName,
                         ProdPrice = p.ProdPrice,
                         ProdQty = p.ProdQty,
-                        Total =p.Total 
-                }).ToList();
+                        Total = p.Total
+                    }).ToList();
                     datagrid.ItemsSource = Products;
                     LoadGrid_selling();
-                    ClearData();    
+                    ClearData();
 
 
 
@@ -191,34 +172,12 @@ namespace ProjectTWO.Windowss
                     }
                     Sum_label.Content = $"{jami} SUM";
 
-                    //var update = db.products.FirstOrDefault(x => x.ProdId == int.Parse(ID_text.Text));
-                    
-                    //if (int.Parse(ID_text.Text) != null)
-                    //{
-                    //    update.Price -= int.Parse(Price_text.Text);
-                    //    db.products.Update(update);
-                    //    db.SaveChanges();
-                    //    //db.products.Update() = product.ProdPrice - int.Parse(Price_text.Text);
-                    //}
-                    
-                    //using (ProjectDB contexts = new ProjectDB())
-                    //{
-                    //    var products = contexts.products.FirstOrDefault(x => x.ProdId == int.Parse(ID_text.Text));
-
-                    //    products.Price -= int.Parse(Price_text.Text);
-
-                    //    contexts.products.Update(products);
-                    //    contexts.SaveChanges();
-                    //}
-
-                    //LoadGrid();
-
 
 
                     MessageBox.Show("Selling Product Added Successfully");
 
                 }
-                
+
 
 
             }
@@ -228,9 +187,9 @@ namespace ProjectTWO.Windowss
             }
 
 
-            
 
-            
+
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -241,24 +200,15 @@ namespace ProjectTWO.Windowss
 
             Product product = new Product();
 
-           
-            //var update = db.products.FirstOrDefault(x => x.ProdId == int.Parse(ID_text.Text));
 
-            //if (int.Parse(ID_text.Text) != null)
-            //{
-            //    update.Price = product.Price - int.Parse(Price_text.Text);
-            //    update.ProdCat = product.ProdCat;
-            //    update.Name = product.Name;
-            //    update.Quantity = product.Quantity;
-                
-
-
-            //}
-            //LoadGrid_selling();
+            
 
 
         }
 
+        private void ID_text_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
+        }
     }
 }
